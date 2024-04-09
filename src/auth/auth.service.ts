452aaaf2afb42f668cc760460b4df656cf24176d
renan-augusto <renan_acm1992@hotmail.com>
2024-04-09 07:04:25 -0300

    feat(sigin) - sigin logic implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -40,7 +40,32 @@ export class AuthService {
         }
     }
 
-    signin() {
-        return { msg: 'I have signin'};
+    async signin(dto: AuthDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            }
+        })
+
+        if(!user) {
+            throw new ForbiddenException(
+                'Credentials incorrect',
+            )
+        };
+
+        const pwMatches = await argon.verify(
+            user.hash, 
+            dto.password,
+        );
+
+        if(!pwMatches) {
+            throw new ForbiddenException(
+                'Credentials incorrect',
+            );
+        };
+
+        delete user.hash;
+        return user;
+
     }
 }
